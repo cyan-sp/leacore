2fac75bf877529fbd6a30b4861e8bb1fef091420
cyan-sp <cyan.mv@gmail.com>
2025-04-19 10:41:02 -0600

    typo and missing import

--- microservicio-aerolineas/src/main/java/com/gmichel/aerolineas/controllers/AerolineaController.java
+++ microservicio-aerolineas/src/main/java/com/gmichel/aerolineas/controllers/AerolineaController.java
@@ -1,21 +1,22 @@
 package com.gmichel.aerolineas.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmichel.aerolineas.dto.AerolineaDTO;
 import com.gmichel.aerolineas.services.AerolineaService;
 import com.gmichel.commons.controllers.CommonController;
 
+
+//@RequestMapping("/api/v1/aerolineas")
 @RestController
-@RequestMapping("/api/v1/aerolineas")
 public class AerolineaController 
 	extends CommonController<AerolineaDTO, AerolineaService>{
 
 	public AerolineaController(AerolineaService service) {
 		super(service);
 	}
-	
-	
+
+
 
 }
