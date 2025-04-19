ea0d29ab97185948edf65f49569f109211b164bf
cyan-sp <cyan.mv@gmail.com>
2025-04-19 12:03:56 -0600

    refactor(backend): improve CORS configuration and enable Eureka client
    - Change CORS implementation from CorsFilter bean to WebMvcConfigurer
    - Add Netflix Eureka client dependency to microservicio-aerolineas
    - Fix line ending issues in various files
    
    This change enables service discovery via Eureka and implements a more
    maintainable approach to CORS configuration.

--- microservicio-aerolineas/src/main/java/com/gmichel/aerolineas/controllers/AerolineaController.java
+++ microservicio-aerolineas/src/main/java/com/gmichel/aerolineas/controllers/AerolineaController.java
@@ -1,14 +1,11 @@
 package com.gmichel.aerolineas.controllers;
 
-//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gmichel.aerolineas.dto.AerolineaDTO;
 import com.gmichel.aerolineas.services.AerolineaService;
 import com.gmichel.commons.controllers.CommonController;
 
-
-//@RequestMapping("/api/v1/aerolineas")
 @RestController
 public class AerolineaController 
 	extends CommonController<AerolineaDTO, AerolineaService>{
